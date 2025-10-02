0cc1c10e2be59414c3fb9696a946bb1a732aa4a4
Ibrahim karoui <brahimkaroui24@gmail.com>
2025-10-02 16:42:01 +0100

    update requirements.txt and cleanup

--- src/csvtest.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-results_path = r"C:\Users\brahi\Documents\Computer_vision\Image classification weather\runs\classify\train\results.csv"
-results = pd.read_csv(results_path, sep=",", skiprows=1)
-print(results.columns.tolist())
-print(results.head())
